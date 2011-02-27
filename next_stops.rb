1826f5311b4f1e3fb7ce3ab417abf1bcd556e8aa
Daniel Choi <dhchoi@gmail.com>
2011-02-27 14:21:04 -0500

    e

--- next_stops.rb
+++ next_stops.rb
@@ -3,11 +3,12 @@ require 'json'
 
 t = YAML::load(File.read('train_trajectories.yml'))
 
-out =t.map do |line, trips|
-  r = trips.map do |trip|
+res = {}
+out = t.map do |line, trips|
+  res[line] = trips.map do |trip|
     trip[:arriving]
   end
 end
 
-File.open("next_stops.yml", 'w') {|f| f.puts out.to_yaml}
-File.open("next_stops.json", "w")  {|f| f.puts out.to_json}
+File.open("next_stops.yml", 'w') {|f| f.puts res.to_yaml}
+File.open("next_stops.json", "w")  {|f| f.puts res.to_json}
