7f7daaf65bf25b280f0f73771d2e1666844e0676
Daniel Choi <dhchoi@gmail.com>
2011-02-26 19:16:56 -0500

    polylines reducer

---
+++ polylines.rb
@@ -0,0 +1,12 @@
+require 'json'
+require 'yaml'
+require 'open-uri'
+res = JSON.parse(open("http://civicapi.com/bos_rail/all").read)
+r = {}
+res["features"].each do |f|
+  p = f["properties"]['properties']
+  g = f["geometry"]["coordinates"]
+  r[p["LINE"]] = {:shape => p["Shape_len"], :geometry => g}
+end
+File.open('polylines.yml', 'w') {|f| f.write r.to_yaml}
+File.open('polylines.json', 'w') {|f| f.write r.to_json}
