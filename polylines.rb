473c4a81c1f9bd90ef2d896ee6f44ca92facb2be
Daniel Choi <dhchoi@gmail.com>
2011-02-27 08:38:31 -0500

    find vertices that are stops

--- polylines.rb
+++ polylines.rb
@@ -5,8 +5,8 @@ res = JSON.parse(open("http://civicapi.com/bos_rail/all").read)
 r = {}
 res["features"].each do |f|
   p = f["properties"]['properties']
-  g = f["geometry"]["coordinates"].flatten
-  (r[p["LINE"]] ||= []) << {'len' => p["Shape_len"], 'geo' => g}
+  g = f["geometry"]["coordinates"]
+  (r[p["LINE"]] ||= []) << {:shape => p["Shape_len"], :geometry => g}
 end
 File.open('polylines.yml', 'w') {|f| f.write r.to_yaml}
 File.open('polylines.json', 'w') {|f| f.write r.to_json}
---
+++ polylines_stops.rb
@@ -0,0 +1,33 @@
+require 'json'
+require 'yaml'
+
+geo_stops = YAML::load(open("geo_subway_stops.yml").read)
+polylines = YAML::load(open("polylines.yml").read)
+
+polylines.each do |line, segs|
+  puts line
+  puts "  %s segments " % segs.size
+  puts "  %s vertices " % segs.reduce(0) {|sum, s| sum + s[:geometry].size}
+  puts "  %s stops" % geo_stops[line].size
+  # match vertices to stops
+  # create a dictionary from geo_subway_stops keyed by lat,lng
+  dict = {}
+  geo_stops[line].each do |stop|
+    lat = stop[1]
+    lng = stop[2]
+    name = stop[0]
+    dict[ [line, lat, lng] ] = name
+  end
+  puts "  Created geo stop dictionary. Matching..."
+  segs.each do |seg|
+    # puts seg.inspect
+    seg[:geometry].each do |vertex|
+      puts vertex.inspect
+      key = [ line, vertex[0], vertex[1] ]
+      if dict[ key ]
+        puts "  Found match for #{dict[key]}"
+      end
+    end
+  end
+end
+
