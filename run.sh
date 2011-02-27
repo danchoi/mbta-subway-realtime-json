0ac13623fcc090176788f6189f1bd0350f76ff53
Daniel Choi <dhchoi@gmail.com>
2011-02-27 13:58:10 -0500

    edit

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ruby geo_subway_stops.rb
-ruby polylines.rb
-ruby polylines_stops.rb
+#ruby geo_subway_stops.rb
+#ruby polylines.rb
+#ruby polylines_stops.rb
 
 ruby subway_predictions.rb
 ruby train_locations.rb
